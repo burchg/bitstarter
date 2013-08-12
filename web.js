140476460047b6ae166a2410a1653f4dd5be652f
burchg <burchg@me.com>
2013-08-12 13:26:15 +0000

    update marketing message

--- web.js
+++ web.js
@@ -1,28 +1,14 @@
-/5000/8080/g
 var express = require('express');
-/5000/8080/g
 var fs = require("fs");
-/5000/8080/g
 
-/5000/8080/g
 var app = express.createServer(express.logger());
-/5000/8080/g
 
-/5000/8080/g
 app.get('/', function(request, response) {
-/5000/8080/g
 	var data = fs.readFileSync("index.html", "utf8");
-/5000/8080/g
   	response.send(data);
-/5000/8080/g
 });
-/5000/8080/g
 
-/5000/8080/g
-var port = process.env.PORT || 5000;
-/5000/8080/g
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
-/5000/8080/g
   console.log("Listening on " + port);
-/5000/8080/g
 });
